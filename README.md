b6e8f22808bf3743dcb4ab7bcecc28c624fea631
Prawana Perera <prawana.perera@gmail.com>
2021-11-08 16:44:42 +1000

    wip

--- README.md
+++ README.md
@@ -1,22 +1,43 @@
-# todos-api
+# TODOsApi
+
 BE for TODOs app
+
+Learning objectives:
+
+- Infra as code on AWS (using CDK)
+- Design of NoSQL db, using single table pattern
+- Graph QL and using managed GraphQL
+- Serverless architectures on AWS
+- Authentication and Authorization on AWS using Cognito
+- Setup CI/CD pipelines (GitHub Actions) for BE code + infra deployed
 
 ## Useful links
 
 ### DynamoDB
+
 - https://edward-huang.com/best-practice/database/2021/04/13/how-to-model-any-relational-data-in-dynamodb-to-maximize-performance/
 - https://www.sensedeep.com/blog/posts/2021/dynamodb-singletable-design.html
 - https://aws.amazon.com/blogs/compute/creating-a-single-table-design-with-amazon-dynamodb/
 - https://www.serverlesslife.com/DynamoDB_Design_Patterns_for_Single_Table_Design.html
+- https://www.profit4cloud.nl/blog/designing-for-amazon-dynamodb/
+- Use KSUID instead of GUUID - https://www.npmjs.com/package/ksuid
 
 ### AppSync + DynamoDB Design
+
 - https://www.alexdebrie.com/posts/dynamodb-single-table/#graphql--single-table-design
 - https://benoitboure.com/how-to-use-dynamodb-single-table-design-with-appsync
 
 ### GraphQL
+
 - https://engineering.zalando.com/posts/2021/04/modeling-errors-in-graphql.html
 - https://blog.logrocket.com/handling-graphql-errors-like-a-champ-with-unions-and-interfaces/
 
 ### AppSync + Auth
+
 - https://github.com/dabit3/build-an-authenticated-api-with-cdk
 - https://github.com/deekob/appsync_reference
+
+### Serverless
+
+- https://event-driven-architecture.workshop.aws/2-event-bridge.html
+- https://serverlessland.com/
--- deploy/todos-api/lib/todos-api-stack.ts
+++ deploy/todos-api/lib/todos-api-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as appsync from '@aws-cdk/aws-appsync';
 import * as ddb from '@aws-cdk/aws-dynamodb';
 import * as lambda from '@aws-cdk/aws-lambda';
+import { RemovalPolicy } from '@aws-cdk/core';
 
 // https://aws.amazon.com/blogs/mobile/building-scalable-graphql-apis-on-aws-with-cdk-and-aws-appsync/
 export class TodosApiStack extends cdk.Stack {
@@ -16,12 +17,100 @@ export class TodosApiStack extends cdk.Stack {
         defaultAuthorization: {
           authorizationType: appsync.AuthorizationType.API_KEY,
           apiKeyConfig: {
-            expires: cdk.Expiration.atDate(new Date(Date.UTC(2021, 12, 31, 0, 0, 0))),
+            expires: cdk.Expiration.atDate(
+              new Date(Date.UTC(2021, 12, 31, 0, 0, 0))
+            ),
           },
         },
       },
       xrayEnabled: true,
     });
+
+    // Create Lambda
+    const todosApiLambda = new lambda.Function(this, 'TodosApiHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('../../lib/todos-api-handler'),
+      memorySize: 1024,
+    });
+
+    // Set the new Lambda function as a data source for the AppSync API
+    const lambdaDs = api.addLambdaDataSource(
+      'TodosApiHandlerDs',
+      todosApiLambda
+    );
+
+    // Add resolver to each query/mutation/subscription
+    lambdaDs.createResolver({
+      typeName: 'Query',
+      fieldName: 'getById',
+    });
+
+    lambdaDs.createResolver({
+      typeName: 'Query',
+      fieldName: 'getAll',
+    });
+
+    lambdaDs.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'create',
+    });
+
+    lambdaDs.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'update',
+    });
+
+    lambdaDs.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'delete',
+    });
+
+    // TODO: check subscription resolvers
+
+    const todosTable = new ddb.Table(this, 'TodosTable', {
+      tableName: 'TodosTable',
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: 'pk',
+        type: ddb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'sk',
+        type: ddb.AttributeType.STRING,
+      },
+    });
+
+    todosTable.addGlobalSecondaryIndex({
+      indexName: 'gsi1',
+      partitionKey: {
+        name: 'pk',
+        type: ddb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'gsi1sk',
+        type: ddb.AttributeType.STRING,
+      },
+    });
+
+    todosTable.addGlobalSecondaryIndex({
+      indexName: 'gsi2',
+      partitionKey: {
+        name: 'pk',
+        type: ddb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'gsi2sk',
+        type: ddb.AttributeType.STRING,
+      },
+    });
+
+    // enable the Lambda function to access the DynamoDB table (using IAM)
+    todosTable.grantReadWriteData(todosApiLambda);
+
+    // Create an environment variable that we will use in the function code
+    todosApiLambda.addEnvironment('TODOS_TABLE', todosTable.tableName);
 
     // Prints out the AppSync GraphQL endpoint to the terminal
     new cdk.CfnOutput(this, 'GraphQLApiUrl', {
